0b1e47b6395becac7d24314ab1a71d6dee2c41e4
Throughyourworld <133323393+Throughyourworld@users.noreply.github.com>
2023-06-30 09:46:47 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -25,7 +25,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oOFvf6i0KmsJ8PMmV1M-YMwQ-l5E',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'uuF02Ig6aKt4rwKKDThafMLl2hguo-Ano1QTMJHD2aA',
+      useTemplateId: '9z4q6EJlxdKOqR6WMppbX-ZwrazxCGu4okW9MQzs_bQ',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '05-03',
       festivals: [
