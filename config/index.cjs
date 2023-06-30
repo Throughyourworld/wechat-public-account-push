7d632d6b0124a5eb431a222222e6a97366cdf9bf
Throughyourworld <133323393+Throughyourworld@users.noreply.github.com>
2023-06-30 10:04:54 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝宝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oOFvf6i0KmsJ8PMmV1M-YMwQ-l5E',
+      id: 'oOFvf6o9nqtKgkCyyttnyeW3NzeA',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'Wsurbi6yOH4KzNmDAf8Ws0YJiw1vugLMQvW_A967Dm8',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
@@ -53,7 +53,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
+  CALLBACK_TEMPLATE_ID: 'Wsurbi6yOH4KzNmDAf8Ws0YJiw1vugLMQvW_A967Dm8',
 
   CALLBACK_USERS: [
     {
